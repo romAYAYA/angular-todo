c3ed97f8bf1290e31f27abb234ac289ff64b1290
xursand7777 <khursand.khaitbaev@mail.ru>
2022-05-10 15:07:32 +0500

    second task2 implemented partly...

--- todo_frontend/src/app/tasks/task02/task02.component.ts
+++ todo_frontend/src/app/tasks/task02/task02.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Task02Service } from './task02.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-task02',
@@ -9,8 +10,13 @@ import { Task02Service } from './task02.service';
   providers: [Task02Service]
 })
 export class Task02Component {
+  public numbers$$: Observable<number[]> = this.store.numbers$$;
+  public countOfRandomNumbers$$: Observable<number> = this.store.countOfRandomNumbers$$;
 
-  constructor() { }
+  constructor(private store: Task02Service) { }
 
+  onClick(): void {
+    this.store.addRandomNumber();
+  }
 }
 
--- todo_frontend/src/app/tasks/task02/task02.service.ts
+++ todo_frontend/src/app/tasks/task02/task02.service.ts
@@ -1,26 +1,41 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, map, Observable } from 'rxjs';
 
 export interface IStore {
-  randomNumbers: number[]
+  randomNumbers: number[],
+  countOfRandomNumbers: number
 }
 
 @Injectable()
 export class Task02Service {
-  // убери все ts-ignore при выполнении задания
 
-  // @ts-ignore
+
   private _store: BehaviorSubject<IStore> = new BehaviorSubject<IStore>({
-    randomNumbers: []
+    randomNumbers: [],
+    countOfRandomNumbers: 0
   });
 
+  public numbers$$:Observable<number[]>= this._store.pipe(
+    map((store) => store.randomNumbers)
+  )
+
+  public countOfRandomNumbers$$: Observable<number>= this._store.pipe(
+    map((store) => store.countOfRandomNumbers)
+  )
 
   public addRandomNumber(): void {
-
+    const randomNumber = this._getRandomNumber();
+    const currentNumbers = this._store.getValue().countOfRandomNumbers + 1;
+    this._store.getValue().randomNumbers.push(randomNumber);
+    this._updateStore({ countOfRandomNumbers: currentNumbers })
   }
 
-  // @ts-ignore
+
   private _getRandomNumber(max: number = 100): number {
     return Math.floor(Math.random() * max);
+  }
+  // @ts-ignore
+  private _updateStore(data: Partial<IStore>): void {
+    this._store.next({ ...this._store.getValue(), ...data });
   }
 }
